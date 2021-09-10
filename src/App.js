421dc2199038cd8fe7d20f2eb02de1772f258263
Antonio Sebastiano <sebastianoa@hotmail.com>
2021-09-10 13:14:26 +1000

    add footer and change arrow start direction

--- src/App.js
+++ src/App.js
@@ -4,106 +4,19 @@ import Header from "./components/Header";
 import AddRecipe from "./components/AddRecipe";
 import ShowAddForm from "./components/ShowAddForm";
 import Recipes from "./components/Recipes";
+import Footer from "./components/Footer";
 
 import { useState, useEffect } from "react";
 
 function App() {
   const [showAddRecipe, setShowAddRecipe] = useState(false);
 
-  const [recipes, setRecipes] = useState([
-    // {
-    //   id: 1,
-    //   recipeName: "Lasagna",
-    //   prepTime: "20 mins",
-    //   cookingTime: "30 mins",
-    //   serves: 4,
-    //   ingredients: [
-    //     {
-    //       ingId: 4,
-    //       qty: 200,
-    //       unit: "ml",
-    //       ingName: "tomato",
-    //     },
-    //     {
-    //       ingId: 6,
-    //       qty: 200,
-    //       unit: "g",
-    //       ingName: "bechamel",
-    //     },
-    //     {
-    //       ingId: 7,
-    //       qty: 300,
-    //       unit: "g",
-    //       ingName: "cheese",
-    //     },
-    //   ],
-    //   method:
-    //     "Create a layer of the lasagna with tomato ragú, bechamel and cheese untul the top of the tray.",
-    // },
-    // {
-    //   id: 2,
-    //   recipeName: "Spaghetti",
-    //   prepTime: "20 mins",
-    //   cookingTime: "30 mins",
-    //   serves: 4,
-    //   ingredients: [
-    //     {
-    //       ingId: 1,
-    //       qty: 200,
-    //       unit: "ml",
-    //       ingName: "tomato",
-    //     },
-    //     {
-    //       ingId: 2,
-    //       qty: 200,
-    //       unit: "g",
-    //       ingName: "bechamel",
-    //     },
-    //     {
-    //       ingId: 3,
-    //       qty: 300,
-    //       unit: "g",
-    //       ingName: "cheese",
-    //     },
-    //   ],
-    //   method:
-    //     "Create a layer of the lasagna with tomato ragú, bechamel and cheese until the top of the tray.",
-    // },
-    // {
-    //   id: 3,
-    //   recipeName: "Gnocchi",
-    //   prepTime: "20 mins",
-    //   cookingTime: "30 mins",
-    //   serves: 4,
-    //   ingredients: [
-    //     {
-    //       ingId: 7,
-    //       qty: 200,
-    //       unit: "ml",
-    //       ingName: "tomato",
-    //     },
-    //     {
-    //       ingId: 8,
-    //       qty: 200,
-    //       unit: "g",
-    //       ingName: "bechamel",
-    //     },
-    //     {
-    //       ingId: 9,
-    //       qty: 300,
-    //       unit: "g",
-    //       ingName: "cheese",
-    //     },
-    //   ],
-    //   method:
-    //     "Create a layer of the lasagna with tomato ragú, bechamel and cheese untul the top of the tray.",
-    // },
-  ]);
+  const [recipes, setRecipes] = useState([]);
 
   // Only run once the first time the component is rendered
   useEffect(() => {
-    if(localStorage.getItem("recipesData")) {
-        setRecipes(JSON.parse(localStorage.getItem("recipesData")))
+    if (localStorage.getItem("recipesData")) {
+      setRecipes(JSON.parse(localStorage.getItem("recipesData")));
     }
   }, []);
 
@@ -125,17 +38,29 @@ function App() {
   };
 
   return (
-    <div>
+    <>
       <Header />
-      <ShowAddForm onAdd={() => setShowAddRecipe(!showAddRecipe)}  showAddRecipe={showAddRecipe}/>
-      {showAddRecipe && <AddRecipe key={recipes.id} addRecipe={addRecipe} setShowAddRecipe={setShowAddRecipe}/>}
-      <Recipes
-        recipes={recipes}
-        setRecipes={setRecipes}
-        deleteRecipe={deleteRecipe}
-        key={recipes.id}
+      <ShowAddForm
+        onAdd={() => setShowAddRecipe(!showAddRecipe)}
+        showAddRecipe={showAddRecipe}
       />
-    </div>
+      {showAddRecipe && (
+        <AddRecipe
+          key={recipes.id}
+          addRecipe={addRecipe}
+          setShowAddRecipe={setShowAddRecipe}
+        />
+      )}
+      <div className="main-container">
+        <Recipes
+          recipes={recipes}
+          setRecipes={setRecipes}
+          deleteRecipe={deleteRecipe}
+          key={recipes.id}
+        />
+        <Footer />
+      </div>
+    </>
   );
 }
 
