1f478a2c17c1726e7db5dbcb38db4ca24dbbf74d
Antonio Sebastiano <sebastianoa@hotmail.com>
2021-09-10 13:21:20 +1000

    change footer paddings

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import Row from "react-bootstrap/Row";
+
+function Footer() {
+  return (
+    <footer className="main-footer py-2">
+      <div className="d-flex justify-content-center text-center align-items-center">
+        <p className="mb-0">My Recipe Book &reg; {new Date().getFullYear()} | Made with love by 
+        <a
+          href="https://www.linkedin.com/in/antonio-sebastiano/"
+          target="_blank"
+          className="ms-1 f-bold"
+        >
+          Antonio Sebastiano
+        </a></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
